604107fe7587c32d3b9c27a6e336a7eb87ca1db2
Olivia Joy Cacdac <127996216+oliviajoyyy@users.noreply.github.com>
2025-04-19 21:26:33 -0700

    Create page to upload posts, implement form for user input for posts

---
+++ client/app/profile/upload/page.tsx
@@ -0,0 +1,178 @@
+'use client'
+import React from 'react'
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+const Checkbox = ({ isChecked, label, checkHandler, index }: { isChecked: any, label: String, checkHandler: any, index: any}) => {
+  return (
+      <div>
+        <input
+          type="checkbox"
+          id={`checkbox=${index}`}
+          value={isChecked}
+          onChange={checkHandler}
+          />
+          <label htmlFor="checkbox">{label}</label>
+      </div>
+  );
+};
+
+  // Item categories
+  const item_categories = [
+    { label: "Women's", checked: false},
+    { label: "Men's", checked: false},
+    { label: "Shirts", checked: false},
+    { label: "Pants", checked: false},
+    { label: "Dresses", checked: false},
+    { label: "Shoes", checked: false},
+    { label: "Hats", checked: false},
+    { label: "Jackets", checked: false},
+  ];
+
+// This page is for uploading a post
+// Saves post information to database
+function UploadPost() {
+  const [closet_id, setClosetID] = useState('');
+  const [category_id, setCategoryID] = useState('');
+  const [title, setTitle] = useState('');
+  const [item_picture, setPicturePath] = useState('');
+  const [description, setDescription] = useState('');
+  const [date_posted, setDate] = useState(getFormattedDate());
+  const [clothing_category, setCategory] = useState('');
+  const [item_condition, setCondition] = useState('');
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [isChecked, setIsChecked] = useState(false);
+  const [categoriesBox, setCategoryCheckbox] = useState(item_categories);
+
+  const router = useRouter(); // for page redirect
+  const { user, error, isLoading } = useUser(); // get this user
+
+  // Item conditions
+  const new_condition = {
+    field: "new", // 'field' is what gets saved to the database in the item_condition field
+    desc: "New - Never worn",
+  }
+  const used_excellent = {
+    field: "excellent",
+    desc: "Used - Excellent",
+  }
+  const used_good = {
+    field: "good",
+    desc: "Used - Good",
+  }
+  const used_fair = {
+    field: "fair",
+    desc: "Used - Fair",
+  }
+
+
+
+  if (!user) {
+    return; // ensure user is logged in
+  }
+
+  
+  function getFormattedDate() {
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const yr = date.getFullYear();
+    const formattedDate = `${yr}-${month}-${day}`
+    console.log(formattedDate);
+    return formattedDate;
+  }
+
+  // Handler for checkbox
+  const onHandleChange = (index: any) => {
+    console.log({index});
+    setCategoryCheckbox(
+      categoriesBox.map((cat, currentIx) => {
+      return currentIx === index
+      ? {...cat, checked: !cat.checked}
+      : cat;
+    }));
+
+  };
+
+  // Submitting the form saves the information to the database through POST
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+
+    // post information to be stored
+    const postInfo = { closet_id: 0, owner_id: user.email, category_id, title, likes: 0, item_picture, description, date_posted, item_condition};
+
+    // send data through POST
+    const response = await fetch('http://localhost:8800/api/profile/upload-post', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(postInfo),
+    });
+
+    const result = await response.json();
+
+    // redirect to profile page when user submits form
+    if (response.ok) {
+      console.log('Post saved:', result);
+      router.push("/profile");
+    } else {
+      console.log('Error:', result);
+    }
+  };
+
+  return (
+    <main>
+      <h1>Post an Item</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <br></br>
+
+        <input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+
+        <br></br>
+
+        <select id="myDropdown" value={item_condition} onChange={(e) => setCondition(e.target.value)}>
+            <option value="">Select Item Condition</option>
+            <option value={new_condition.field}>{new_condition.desc}</option>
+            <option value={used_excellent.field}>{used_excellent.desc}</option>
+            <option value={used_good.field}>{used_good.desc}</option>
+            <option value={used_fair.field}>{used_fair.desc}</option>
+        </select>
+        {/* <p>You selected: {item_condition}</p> */}
+
+        <br></br>
+
+        <div>
+          {categoriesBox.map((cat, index) => {
+            console.log({index});
+            return <Checkbox
+              isChecked={cat.checked}
+              label={cat.label}
+              checkHandler={() => onHandleChange(index)}
+              index={index}
+            />
+          })}
+
+        
+        </div>
+        <br></br>
+
+        <button type="submit">Upload Post</button>
+      </form>
+    </main>
+  );
+}
+
+export default UploadPost;
