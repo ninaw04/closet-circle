eb65e390476cbf475ca73aac39d4a2615bb458dc
oliviajoyyy <127996216+oliviajoyyy@users.noreply.github.com>
2025-04-30 12:15:11 -0700

    Implement saving post to db from profile page, using updated db

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -40,7 +40,7 @@ const PLACEHOLDER: ClosetProduct = {
     forSale   : true,
     forRent   : true,
     type      : 'Dresses',
-    audience  : 'Womens',
+    audience  : "Women's",
     colors    : ['white'],
     sizes     : ['Small'],
     condition : 'Brand new',
@@ -504,7 +504,7 @@ const emptyProduct: NewProduct = {
     forSale: true,
     forRent: false,
     type  : 'Tops',
-    audience: 'Womens',
+    audience: "Women's",
     colors: [],
     sizes : [],
     condition: conditionOptions[0],
@@ -624,7 +624,7 @@ export const AddProductModal: React.FC<AddProductModalProps> = ({ open, onClose,
                             value={draft.audience}
                             onChange={e => update('audience', e.target.value as NewProduct['audience'])}
                         >
-                            {['Mens','Womens','Kids'].map(a=>(
+                            {["Men's","Women's","Kids"].map(a=>(
                                 <option key={a}>{a}</option>
                             ))}
                         </select>
@@ -733,7 +733,7 @@ export const AddProductModal: React.FC<AddProductModalProps> = ({ open, onClose,
                             const errs = validate(draft);
                             if (Object.keys(errs).length) { setErrors(errs); return; }
                             //console.log(draft);
-                            console.log(draft.audience);
+                            // console.log(draft.audience);
                             onSave(draft);
                             resetAndClose();
                         }}
@@ -874,31 +874,59 @@ const ProfilePage: React.FC = () => {
         return formattedDate;
     }
 
-    const db_categories = [
-        { label: "Womens", checked: false, db_val: 6 },
-        { label: "Mens", checked: false, db_val: 7 },
-        { label: "Kids", checked: false, db_val: 8 },
-        { label: "Tops", checked: false, db_val: 1 },
-        { label: "Bottoms", checked: false, db_val: 4 },
-        { label: "Outerwear", checked: false, db_val: 3 },
-        { label: "Dresses", checked: false, db_val: 5 },
-        { label: "Shoes", checked: false, db_val: 2 },
-        { label: "Accessories", checked: false, db_val: 9 },
-        { label: "Black", checked: false, db_val: 10 },
-        { label: "White", checked: false, db_val: 11 },
-        { label: "Red", checked: false, db_val: 12 },
-        { label: "Blue", checked: false, db_val: 13 },
-        { label: "Pink", checked: false, db_val: 14 },
+    const dbCategories = [
+        { label: "Women's", db_val: 1 },
+        { label: "Men's", db_val: 2 },
+        { label: "Kids", db_val: 3 },
+        { label: "Tops", db_val: 4 },
+        { label: "Bottoms", db_val: 5 },
+        { label: "Outerwear", db_val: 6 },
+        { label: "Dresses", db_val: 7 },
+        { label: "Shoes", db_val: 8 },
+        { label: "Accessories", db_val: 9 },
+        { label: colorOptions[0], db_val: 10 },
+        { label: colorOptions[1], db_val: 11 },
+        { label: colorOptions[2], db_val: 12 },
+        { label: colorOptions[3], db_val: 13 },
+        { label: colorOptions[4], db_val: 14 },
+        { label: colorOptions[5], db_val: 15 }
     ];
 
-    function getDBCategories(type: any, audience: any) {
-        var db: any = [];
-        db_categories.forEach(cat => {
+    const dbConditionVals = [
+        { label: conditionOptions[0], db_val: "new"},
+        { label: conditionOptions[1], db_val: "excellent"},
+        { label: conditionOptions[2], db_val: "good"},
+        { label: conditionOptions[3], db_val: "worn"},
+    ];
+
+    function getDBCategories(type: any, audience: any, colors: string[] | undefined) {
+        var dbCategoryID: any = [];
+        dbCategories.forEach(cat => {
+            // find type and audience (1 type and 1 audience can be selected)
             if (type == cat.label || audience == cat.label) {
-                db.push(cat.db_val);
+                dbCategoryID.push(cat.db_val);
+            }
+
+            if (colors != undefined) {
+                // find colors (multiple colors can be selected)
+                colors.forEach(c  => {
+                    if (c == cat.label) {
+                        dbCategoryID.push(cat.db_val);
+                    }
+                })
             }
         });
-        return db;
+        return dbCategoryID;
+    }
+
+    function getDBCondition(condition: string | undefined) {
+        var dbCondition;
+        dbConditionVals.forEach(conditionOption => {
+            if (condition == conditionOption.label) {
+                dbCondition = conditionOption.db_val;
+            }
+        });
+        return dbCondition;
     }
 
     /* page */
@@ -1083,14 +1111,15 @@ const ProfilePage: React.FC = () => {
                                     owner_id: user.email,
                                     title: p.title,
                                     likes: 0,
-                                    item_picture: p.images[0], // db currently stores only 1 image
+                                    item_pictures: p.images, // db currently stores only 1 image
                                     description: p.description,
                                     date_posted: getFormattedDate(),
-                                    item_condition: p.condition,
-                                    categories: getDBCategories(p.type, p.audience), // array of category_id
+                                    item_condition: getDBCondition(p.condition), // return either 'new', 'excellent', 'good', 'worn'
+                                    categories: getDBCategories(p.type, p.audience, p.colors), // array of category_id
                                     size: p.sizes? p.sizes[0] : p.sizes, // TODO update if changing to not an array
                                     for_sale: p.forSale,
-                                    for_rent: p.forRent
+                                    for_rent: p.forRent,
+                                    price: p.price
                                 };
 
                                 // 1) send to backend
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -23,22 +23,40 @@ module.exports = (db) => {
     // POST post for a specific user - O.C.
     router.post("/upload-item", (req, res) => {
         var post_id;
-        const { closet_id, owner_id, title, likes, item_picture, description, date_posted, item_condition, categories, size, for_sale, for_rent } = req.body;
-        const query = `
-        INSERT INTO Post (closet_id, owner_id, title, likes, item_picture, description, date_posted, item_condition) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-    `;
+        const { closet_id, owner_id, title, likes, item_pictures, description, date_posted, item_condition, categories, size, for_sale, for_rent, price} = req.body;
+        const query = `INSERT INTO Post (closet_id, owner_id, title, likes, description, date_posted, item_condition, size) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
         const query_post_category = `INSERT INTO Post_Category (post_id, category_id) VALUES (?, ?)`;
+        const query_post_images = `INSERT INTO Post_Image (post_id, image_url) VALUES (?, ?)`;
+        const query_borrow = `INSERT INTO Borrow (post_id, rental_start_date, rental_end_date) VALUES (?, ?, ?)`;
+        const query_sell = `INSERT INTO Sell (post_id, price) VALUES (?, ?)`;
         console.log(categories);
 
         db.run(
             query,
-            [closet_id, owner_id, title, likes, item_picture, description, date_posted, item_condition],
+            [closet_id, owner_id, title, likes, description, date_posted, item_condition, size],
             function (err) {
                 if (err) {
+                    console.log("error inserting inital post");
+                    console.log([closet_id, owner_id, title, likes, description, date_posted, item_condition, size]);
                     res.status(500).json({ error: err.message });
                     return;
                 }
                 post_id = this.lastID;
+
+                // Associate post with multiple images
+                item_pictures.forEach(image_url => {
+                    db.run(
+                        query_post_images,
+                        [post_id, image_url],
+                        function (err) {
+                            if (err) {
+                                console.log("error inserting images");
+                                res.status(500).json({ error: err.message });
+                                return;
+                            }
+                        }
+                    )
+                });
 
                 // Associate post with each category that was selected - O.C.
                 // categories is an array of integers representing the category_id
@@ -48,12 +66,43 @@ module.exports = (db) => {
                             [post_id, category_id],
                             function (err) {
                                 if (err) {
+                                    console.log("error inserting category");
                                     res.status(500).json({ error: err.message });
                                     return;
                                 }
                             }
                         )
                 });
+
+                // Borrow post
+                if (for_rent) {
+                    db.run(
+                        query_borrow,
+                        [post_id, "2025-05-01", "2020-05-07"],
+                        function (err) {
+                            if (err) {
+                                console.log("error inserting rent post");
+                                res.status(500).json({ error: err.message });
+                                return;
+                            }
+                        }
+                    )
+                }
+
+                // Sell post
+                if (for_sale) {
+                    db.run(
+                        query_sell,
+                        [post_id, price],
+                        function (err) {
+                            if (err) {
+                                console.log("error inserting sell post");
+                                res.status(500).json({ error: err.message });
+                                return;
+                            }
+                        }
+                    )
+                }
 
                 console.log("size: " + size + ", for sale: " + for_sale + ", for rent: " + for_rent);
 
